f38db5a1a489a5568691c032f4e688614fec3a21
Piotr Wasiak <chooosen@gmail.com>
2017-01-30 02:18:15 +0100

    Rubocop code

--- chat.rb
+++ chat.rb
@@ -1,29 +1,30 @@
 require 'json'
 require 'bunny'
+# Console chat client
 class Chat
-
   def display_message(user, message)
     puts "#{user}: #{message}"
   end
 
   def initialize
-    print "Type in your name: "
+    print 'Type in your name: '
     @current_user = gets.strip
-    puts "Hi #{@current_user}, you just joined a chat room! Type your message in and press enter."
+    puts "Hi #{@current_user}, you just joined a chat room!" \
+         ' Type your message in and press enter.'
 
     conn = Bunny.new
     conn.start
 
     @channel = conn.create_channel
-    @exchange = @channel.fanout("super.chat")
+    @exchange = @channel.fanout('super.chat')
 
     listen_for_messages
   end
 
   def listen_for_messages
-    queue = @channel.queue("super.chat")
+    queue = @channel.queue('super.chat')
 
-    queue.bind(@exchange).subscribe do |delivery_info, metadata, payload|
+    queue.bind(@exchange).subscribe do |_delivery_info, _metadata, payload|
       data = JSON.parse(payload)
       display_message(data['user'], data['message'])
     end
